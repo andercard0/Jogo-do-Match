f03488260c5dae3252fda6728c1f6b4de030c14a
Anderson Cardoso <43047877+andercard0@users.noreply.github.com>
2023-12-27 12:23:42 -0300

    Temporizador
    Adicionado temporizador ao campo da grade do programa.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,15 +15,31 @@ using System.Windows.Shapes;
 
 namespace MatchGame
 {
-    /// <summary>
-    /// Lógica de interatividade com a MainWindow.xaml
-    /// </summary>
+    using System.Windows.Threading;
+    
+    /// Lógica de interatividade com a MainWindow.xaml    
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        int matchesFound;
         public MainWindow()
         {
             InitializeComponent();
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
             SetUpGame();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tenthsOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+            if (matchesFound == 8)
+            {
+                timer.Stop();
+                timeTextBlock.Text = timeTextBlock.Text + " - Play again?";
+            }
         }
 
         //Método
@@ -43,16 +59,26 @@ namespace MatchGame
 
             //Recurso do emoji em emojipedia.org
             //To do: carregamento de fontes/ícones ou svgs externos não diretamente incorporados no programa.
+            //To do: assim que o jogo terminar, soar um som 'ta-da' genérico do sistema.
 
             Random random = new Random();
 
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
-                int index = random.Next(animalEmoji.Count);
-                string nextEmoji = animalEmoji[index];
-                textBlock.Text = nextEmoji;
-                animalEmoji.RemoveAt(index);
+                if (textBlock.Name != "timeTextBlock") //Gambiarra de exceção para a adição do bloco de texto adicionado não dar merda.
+                {
+                    textBlock.Visibility = Visibility.Visible;
+                    int index = random.Next(animalEmoji.Count);
+                    string nextEmoji = animalEmoji[index];
+                    textBlock.Text = nextEmoji;
+                    animalEmoji.RemoveAt(index);
+                }
             }
+
+            //Método do temporizador para iniciar a contagem e redefinir os quadros
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            matchesFound = 0;
         }
 
         //Manipulador do eventos do mouse (clique)
@@ -79,5 +105,13 @@ namespace MatchGame
             }
         }
 
+        //Evento de temporizador ao clicar em uma imagem
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(matchesFound == 8)
+            {
+                SetUpGame();
+            }
+        }
     }
 }
